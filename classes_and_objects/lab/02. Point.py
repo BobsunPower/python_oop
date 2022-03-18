4a2133752ae6a5ce519b7e87b49013a5bc74145a
BobsunPower <bobsunpower@gmail.com>
2022-03-19 00:02:37 +0200

    Completed

---
+++ classes_and_objects/lab/02. Point.py
@@ -0,0 +1,12 @@
+class Point:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+    def set_x(self, new_x):
+        self.x = new_x
+
+    def set_y(self, new_y):
+        self.y = new_y
+
+    def __str__(self):
+        return f"The point has coordinates ({self.x},{self.y})"
