0fe5d7c12e8544c1037a6f83aaf574981da734d1
BobsunPower <bobsunpower@gmail.com>
2022-03-23 16:23:12 +0200

    Completed

--- classes_and_objects/exercise/guild_system/project/guild.py
+++ classes_and_objects/exercise/guild_system/project/guild.py
@@ -1,1 +1,27 @@
-# TODO
+from project.player import Player
+
+
+class Guild:
+    def __init__(self, name: str):
+        self.name, self.players = name, []
+
+    def assign_player(self, player):
+        if player.guild != "Unaffiliated" and player not in self.players:
+            return f"Player {player.name} is in another guild."
+        if player not in self.players:
+            self.players.append(player)
+            player.guild = self.name
+            return f"Welcome player {player.name} to the guild {self.name}"
+        else:
+            return f"Player {player.name} is already in the guild."
+
+    def kick_player(self, player_name: str):
+        for p in self.players:
+            if p.name == player_name:
+                self.players.remove(p)
+                p.guild = "Unaffiliated"
+                return f"Player {player_name} has been removed from the guild."
+        return f"Player {player_name} is not in the guild."
+
+    def guild_info(self):
+        return f"Guild: {self.name}\n{''.join([p.player_info() for p in self.players])}"
--- classes_and_objects/exercise/guild_system/project/player.py
+++ classes_and_objects/exercise/guild_system/project/player.py
@@ -1,1 +1,17 @@
-# TODO
+class Player:
+    def __init__(self, name: str, hp: int, mp: int):
+        self.name, self.hp, self.mp, self.skills, self.guild = name, hp, mp, {}, "Unaffiliated"
+
+    def add_skill(self, skill_name, mana_cost):
+        if skill_name in self.skills:
+            return "Skill already added"
+        self.skills[skill_name] = mana_cost
+        return f"Skill {skill_name} added to the collection of the player {self.name}"
+
+    def player_info(self):
+        nl = "\n"
+        return f"""Name: {self.name}
+Guild: {self.guild}
+HP: {self.hp}
+MP: {self.mp}
+{nl.join(f'==={k} - {v}' for k, v in self.skills.items())}"""
