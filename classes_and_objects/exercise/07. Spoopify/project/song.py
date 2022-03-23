aa4e2afc09918f25a5a88f10238076ca2c7ac97c
BobsunPower <bobsunpower@gmail.com>
2022-03-23 11:38:21 +0200

    Completed

--- classes_and_objects/exercise/07. Spoopify/project/song.py
+++ classes_and_objects/exercise/07. Spoopify/project/song.py
@@ -1,8 +1,1 @@
-class Song:
-    def __init__(self, name, length, single):
-        self.username = name
-        self.length = length
-        self.single = single
-
-    def get_info(self):
-        return f"{self.username} - {self.length}"
+# TODO
