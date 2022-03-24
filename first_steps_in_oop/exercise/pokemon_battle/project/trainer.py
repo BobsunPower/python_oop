e2ae863227891c67071711489ac09c4ed48dcb5a
BobsunPower <bobsunpower@gmail.com>
2022-03-24 10:57:22 +0200

    Completed

--- first_steps_in_oop/exercise/pokemon_battle/project/trainer.py
+++ first_steps_in_oop/exercise/pokemon_battle/project/trainer.py
@@ -1,3 +1,4 @@
+# from first_steps_in_oop.exercise.pokemon_battle.project.pokemon import Pokemon
 from project.pokemon import Pokemon
 
 
@@ -22,15 +23,3 @@ class Trainer:
         return f"""Pokemon Trainer {self.name}
 Pokemon count {len(self.pokemons)}
 - {' '.join([p.pokemon_details() for p in self.pokemons])}"""
-
-
-pokemon = Pokemon("Pikachu", 90)
-print(pokemon.pokemon_details())
-trainer = Trainer("Ash")
-print(trainer.add_pokemon(pokemon))
-second_pokemon = Pokemon("Charizard", 110)
-print(trainer.add_pokemon(second_pokemon))
-print(trainer.add_pokemon(second_pokemon))
-print(trainer.release_pokemon("Pikachu"))
-print(trainer.release_pokemon("Pikachu"))
-print(trainer.trainer_data())
