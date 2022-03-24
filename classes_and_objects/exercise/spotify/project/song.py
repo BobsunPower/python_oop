00a49415e44db04e305a6989057a7ce348071ad8
BobsunPower <bobsunpower@gmail.com>
2022-03-24 10:24:23 +0200

    Completed

--- classes_and_objects/exercise/spotify/project/song.py
+++ classes_and_objects/exercise/spotify/project/song.py
@@ -1,1 +1,6 @@
-# TODO
+class Song:
+    def __init__(self, name: str, length: float, single: bool):
+        self.name, self.length, self.single = name, length, single
+
+    def get_info(self):
+        return f"{self.name} - {self.length}"
