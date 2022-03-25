ef1c30a404d5a0f18006ca8fb8fdc2a9dd01b9c8
BobsunPower <bobsunpower@gmail.com>
2022-03-25 21:23:39 +0200

    Completed

---
+++ inheritance/exercise/person/project/child.py
@@ -0,0 +1,6 @@
+# from inheritance.exercise.person.project.person import Person
+from project.person import Person
+
+
+class Child(Person):
+    pass
---
+++ inheritance/exercise/person/project/person.py
@@ -0,0 +1,3 @@
+class Person:
+    def __init__(self, name: str, age: int):
+        self.name, self.age = name, age
