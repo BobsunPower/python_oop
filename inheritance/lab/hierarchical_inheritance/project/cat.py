2844a2bc9e7df77d8f56d635c1768e70436105de
BobsunPower <bobsunpower@gmail.com>
2022-03-25 12:04:55 +0200

    Completed

---
+++ inheritance/lab/hierarchical_inheritance/project/cat.py
@@ -0,0 +1,7 @@
+# from inheritance.lab.hierarchical_inheritance.project.animal import Animal
+from project.animal import Animal
+
+
+class Cat(Animal):
+    def meow(self):
+        return "meowing..."
---
+++ inheritance/lab/hierarchical_inheritance/project/dog.py
@@ -0,0 +1,7 @@
+# from inheritance.lab.hierarchical_inheritance.project.animal import Animal
+from project.animal import Animal
+
+
+class Dog(Animal):
+    def bark(self):
+        return "barking..."
