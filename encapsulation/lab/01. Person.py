9c438ba2f776d28f08cff17f18a4f1a0f0a58239
BobsunPower <bobsunpower@gmail.com>
2022-03-29 11:29:52 +0300

    Completed

--- encapsulation/lab/01. Person.py
+++ encapsulation/lab/01. Person.py
@@ -1,9 +1,17 @@
 class Person:
     def __init__(self, name: str, age: int):
-        self._Person__name, self._Person__age = name, age
+        self.__name, self.__age = name, age
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def age(self):
+        return self.__age
 
     def get_name(self):
-        return self._Person__name
+        return self.__name
 
     def get_age(self):
-        return self._Person__age
+        return self.__age
