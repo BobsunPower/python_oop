b783d62aa3dbcc43c05dbd47365351f1da66b161
BobsunPower <bobsunpower@gmail.com>
2022-03-29 12:53:42 +0300

    Completed

--- encapsulation/lab/02. Mammal.py
+++ encapsulation/lab/02. Mammal.py
@@ -2,6 +2,10 @@ class Mammal:
     def __init__(self, name: str, type: str, sound: str):
         self.name, self.type, self.sound, self.__kingdom = name, type, sound, 'animals'
 
+    @property
+    def kingdom(self):
+        return self.__kingdom
+
     def make_sound(self):
         return f"{self.name} makes {self.sound}"
 
