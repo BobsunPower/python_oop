168e98febd189ba15aaee60aa3ca522a03a59848
BobsunPower <bobsunpower@gmail.com>
2022-04-11 19:00:12 +0300

    Completed

--- encapsulation/exercise/football_team_generator/project/player.py
+++ encapsulation/exercise/football_team_generator/project/player.py
@@ -1,1 +1,15 @@
-# TODO
+class Player:
+    def __init__(self, name: str, sprint: int, dribble: int, passing: int, shooting: int):
+        self.__name, self.__sprint, self.__dribble = name, sprint, dribble
+        self.__passing, self.__shooting = passing, shooting
+
+    @property
+    def name(self):
+        return self.__name
+
+    def __str__(self):
+        return f"""Player: {self.__name}
+Sprint: {self.__sprint}
+Dribble: {self.__dribble}
+Passing: {self.__passing}
+Shooting: {self.__shooting}"""
--- encapsulation/exercise/football_team_generator/project/team.py
+++ encapsulation/exercise/football_team_generator/project/team.py
@@ -1,1 +1,20 @@
-# TODO
+# from encapsulation.exercise.football_team_generator.project.player import Player
+from project.player import Player
+
+
+class Team:
+    def __init__(self, name: str, rating: int):
+        self.__name, self.__rating, self.__players = name, rating, []
+
+    def add_player(self, player: Player):
+        if player in self.__players:
+            return f"Player {player.name} has already joined"
+        self.__players.append(player)
+        return f"Player {player.name} joined team {self.__name}"
+
+    def remove_player(self, player_name: str):
+        for player in self.__players:
+            if player.name == player_name:
+                self.__players.remove(player)
+                return player
+        return f"Player {player_name} not found"
