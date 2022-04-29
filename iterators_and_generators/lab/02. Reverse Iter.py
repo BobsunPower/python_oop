da01450e4245e11dfb565b4da543a88d7b625b42
BobsunPower <bobsunpower@gmail.com>
2022-04-29 08:41:14 +0300

    Completed

---
+++ iterators_and_generators/lab/02. Reverse Iter.py
@@ -0,0 +1,15 @@
+class reverse_iter:
+    def __init__(self, iterable):
+        self.iterable, self.starting_point, self.end_point = iterable, len(iterable) - 1, 0
+        self.work_value = self.starting_point
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        temp = self.work_value
+        if self.work_value < self.end_point:
+            raise StopIteration
+
+        self.work_value -= 1
+        return self.iterable[temp]
