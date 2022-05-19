672eead9dfae80bf6d752031da5c20cfcd4c216d
BobsunPower <bobsunpower@gmail.com>
2022-05-19 23:55:57 +0300

    Completed

---
+++ decorators/lab/02. Vowel Filter.py
@@ -0,0 +1,13 @@
+vowels = ["a", "e", "i", "o", "y", "u"]
+
+
+def vowel_filter(function):
+    def wrapper():
+        return [el for el in function() if el.lower() in vowels]
+
+    return wrapper
+
+
+@vowel_filter
+def get_letters():
+    return ["a", "b", "c", "d", "e"]
