24e4a37c52ea97368876bdc1cf8fe597f8620e0a
BobsunPower <bobsunpower@gmail.com>
2022-05-28 21:51:54 +0300

    Completed

---
+++ decorators/exercise/06. HTML Tags.py
@@ -0,0 +1,18 @@
+def tags(tag):
+    def decorator(func):
+        def wrapper(*args):
+            return f"<{tag}>" + func(*args) + f"</{tag}>"
+
+        return wrapper
+
+    return decorator
+
+
+@tags('p')
+def join_strings(*args):
+    return "".join(args)
+
+
+@tags('h1')
+def to_upper(text):
+    return text.upper()
